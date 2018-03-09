7f45aae38fb9fdae24ace7c6d71d23aa36abbd92
Antoine Labrecque <>
2018-03-09 16:18:55 -0600

    Finished the third case.

--- ack.hs
+++ ack.hs
@@ -4,7 +4,8 @@
 -- Ackermann function, parameters must be nonnegative.
 ack :: Integral a => a -> a -> a -> Integer
 ack a b 0 = toInteger ( a + b )
-ack a 0 1 = 0
+ack _ 0 1 = 0
+ack _ 0 2 = 1
 
 -- tests
 -- 0 == ack 0 0 0
@@ -12,3 +13,4 @@ ack a 0 1 = 0
 -- 2 == ack 0 2 0
 -- 0 == ack 0 0 1
 -- 0 == ack 1 0 1
+-- 1 == ack 0 0 2
