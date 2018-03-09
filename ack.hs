f7fa52d6883e67c84176c5a6c4e7706179326c07
Antoine Labrecque <>
2018-03-09 16:15:08 -0600

    Finished the first two cases.

--- ack.hs
+++ ack.hs
@@ -1,8 +1,14 @@
 {-Antoine Labrecque
  -11177990
  -ajl963-}
+-- Ackermann function, parameters must be nonnegative.
 ack :: Integral a => a -> a -> a -> Integer
-ack a b c = 0
+ack a b 0 = toInteger ( a + b )
+ack a 0 1 = 0
 
 -- tests
 -- 0 == ack 0 0 0
+-- 1 == ack 1 0 0
+-- 2 == ack 0 2 0
+-- 0 == ack 0 0 1
+-- 0 == ack 1 0 1
